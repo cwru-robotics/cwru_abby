c544e36110f94ece459f28bda44d0d2a11d8da76
Luc Bettaieb <luc.bettaieb@gmail.com>
2015-07-15 21:09:23 -0700

    removed old stuff from master.  this will be where hydro stuff will go until indigo stuff is developed

--- abby_arm_actions/src/abby_arm_actions/msg/__init__.py
+++
@@ -1,7 +0,0 @@
-from ._ArmActionResult import *
-from ._ArmActionActionGoal import *
-from ._ArmActionGoal import *
-from ._ArmActionActionFeedback import *
-from ._ArmActionAction import *
-from ._ArmActionFeedback import *
-from ._ArmActionActionResult import *
